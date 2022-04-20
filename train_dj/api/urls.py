86c761b8cf8fb3f0d7617c997889c6a98e58aac6
flond7 <elisa.utilivenza@gmail.com>
2022-04-20 17:52:17 +0200

    added basic user urls

--- train_dj/api/urls.py
+++ train_dj/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-  
+  path('user-list', views.userList, name="user-list"),                 
+  path('user-detail/<str:pk>', views.userDetail, name="user-detail"),  
+  path('user-create', views.userCreate, name="user-create"),           
+  path('user-update/<str:pk>', views.userUpdate, name="user-update"),  
+  path('user-delete/<str:pk>', views.userDelete, name="user-delete"),  
 ]
