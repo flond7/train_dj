2733ccb3a60de458fac325b13c4b12f1b4e9ed93
flond7 <elisa.utilivenza@gmail.com>
2022-04-20 16:39:23 +0200

    basic models

--- train_dj/api/models.py
+++ train_dj/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, blank=True, default='')
+    surname = models.CharField(max_length=50, blank=True, default='')
+    mail = models.EmailField(max_length=150)
+    pw = models.CharField(max_length=50)
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(default='')
+    answerOne = models.TextField(default='')
+    answerTwo = models.TextField(default='')
+    answerThree = models.TextField(default='')
+    correct = models.TextField(default='')
+    id_path = models.IntegerField()
+    id_video = models.IntegerField()
+
+class Results(models.Model):
+    id = models.AutoField(primary_key=True)
+    correct = models.BooleanField()
+    id_question = models.IntegerField()
+    id_user = models.IntegerField()  
